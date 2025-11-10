cb21e244d0e2e7321e59427f97855e2350641bc5
l <47887093+lmjpg@users.noreply.github.com>
2025-11-10 20:02:16 +1000

    Play audio instead of saving to a file

--- go.mod
+++ go.mod
@@ -7,4 +7,12 @@ require (
 	github.com/mappu/miqt v0.12.0
 )
 
-require golang.org/x/sys v0.26.0 // indirect
+require (
+	github.com/ebitengine/oto/v3 v3.3.2 // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/gopxl/beep v1.4.1 // indirect
+	github.com/gopxl/beep/v2 v2.1.1 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+)
--- player.go
+++ player.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"log"
 	"net/http"
-	"os"
+	"time"
+
+	"github.com/gopxl/beep/mp3"
+	"github.com/gopxl/beep/speaker"
 )
 
 func getUrl(c chan string) (string, bool) {
@@ -18,58 +20,33 @@ func getUrl(c chan string) (string, bool) {
 	}
 }
 
-func getAudioBuffer(url string) (*bufio.Reader, *bufio.Writer, error) {
+func getAudioBuffer(url string) error {
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, nil, err
-	}
-
-	file, err := os.Create("out.mp3")
-	if err != nil {
-		return nil, nil, err
-	}
-
-	reader := bufio.NewReader(resp.Body)
-	filewriter := bufio.NewWriter(file)
-	return reader, filewriter, nil
-}
-
-func readAudioBuffer(reader *bufio.Reader, filewriter *bufio.Writer) error {
-	size := reader.Size()
-	buf := make([]byte, size)
-	n, err := reader.Read(buf)
-	if err != nil {
 		return err
 	}
 
-	log.Printf("Got %v bytes, len %v\n", n, len(buf))
-	_, err = filewriter.Write(buf[:n])
+	streamer, format, err := mp3.Decode(resp.Body)
 	if err != nil {
 		return err
 	}
+	log.Println(format)
+	log.Println(streamer)
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second))
+	speaker.Play(streamer)
 
 	return nil
 }
 
 func player(c chan string) {
-	var reader *bufio.Reader
-	var filewriter *bufio.Writer
 	for {
 		url, ok := getUrl(c)
 		if ok {
 			log.Printf("Playing from %v\n", url)
-			var err error
-			reader, filewriter, err = getAudioBuffer(url)
+			err := getAudioBuffer(url)
 			if err != nil {
 				log.Println(err)
-			}
-		}
-
-		if reader != nil && filewriter != nil {
-			err := readAudioBuffer(reader, filewriter)
-			if err != nil {
-				log.Println(err)
-				reader, filewriter = nil, nil
 			}
 		}
 	}
